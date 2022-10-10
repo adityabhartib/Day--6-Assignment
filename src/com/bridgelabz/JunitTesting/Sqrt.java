f4f56eb7fdb4b174a155a007161665677ab1e725
adityabhartib <adityabharti.ab@gmail.com>
2022-10-10 14:59:09 +0530

    Vending Machine Added

--- src/com/bridgelabz/JunitTesting/Sqrt.java
+++ src/com/bridgelabz/JunitTesting/Sqrt.java
@@ -7,15 +7,12 @@ public class Sqrt {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter Number: ");
         int n= sc.nextInt();
-        double epsilon = 1.0e-15;  // relative error tolerance
-        double t = n;              // estimate of the square root of c
-
-        // repeatedly apply Newton update step until desired precision is achieved
+        double epsilon = 1.0e-15;
+        double t = n;
         while (Math.abs(t - n / t) > epsilon * t) {
             t = (n / t + t) / 2.0;
         }
 
-        // print out the estimate of the square root of c
         System.out.println(t);
     }
 }
